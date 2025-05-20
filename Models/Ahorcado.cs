4591cf902088e69a04498bb7a4e8dee821e92896
FELIPE FELDMAN <48860060@ort>
2025-05-20 11:50:41 -0300

    arriesgoLetra hecho

--- Models/Ahorcado.cs
+++ Models/Ahorcado.cs
@@ -5,17 +5,39 @@ static public class Ahorcado{
     static public int cantIntentos {get; private set;}
     static public string palabraParcial {get; private set;}
 
+    static public List<char> letrasUsadas = new List<char>();
+
     static public void inicializarGrupo(){
         cantIntentos = 0;
         palabra = "PROVINCIANOS";
         palabraParcial = "------------";
     }
 
-    static public void arriesgoLetra(char letra){
+    static public bool arriesgoLetra(char letra){
+        bool acierto = false;
+        char[] parcial = palabraParcial.ToCharArray();
+        
+         
+        for(int i = 0; i < palabra.Length; i++){
+            if(letra == palabra.Length){
+                acierto = true;
+                parcial[i] = letra;
+                cantIntentos++;
+                letrasUsadas.Add(letra);
+                Console.WriteLine("Correcto!");
+            }else if(!acierto){
+                cantIntentos++;
+                letrasUsadas.Add(letra);
+                Console.WriteLine("Incorrecto, mala suerte!");
+            }
+        }
+        return acierto;
+
+
 
     }
 
-    static public bool arriesgoPalabra(string palabra){
-
-    }
+    //static public bool arriesgoPalabra(string palabra){
+        
+    //}
 }
